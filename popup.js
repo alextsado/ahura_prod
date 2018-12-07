a3433c0c4be1391eb23bc9df6ff159b75578fb5e
Barnaby-at-020885411870 <barnaby@ahuraai.com>
2018-12-07 21:39:12 +0000

    refactoring the code so that all of the routing is centrally located and all of the event handlers can eventually be sent to separate files.

--- popup.js
+++ popup.js
@@ -1,14 +1,18 @@
 /**
  * Control the popup page
+ * TODO separate this into multiple files so that's it's less difficult to manage
  *
  * @Author Barnaby B.
  * @Since Nov 14, 2018
  */
 
+// ------------------------------------------
+// Routing
+// -------------------------------------------------
+
 /*
  * Route any messages relevant to the functionality of the window
  */
-
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     //Message from a newly loaded page
     if(msg.type === "summary_text"){
@@ -21,65 +25,47 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 });
 
 /*
- * TODO get the page ID from the link data, not the session id from the stored information
- *
- * Clicking on a keyword sends an ajax request to make the page legit.
+ * Set up event listeners
  */
-function keyword_click(event){
-    let keyword_link = event.target;
-    let new_keyword = keyword_link.innerText;
-    let page_id = keyword_link.closest(".page_history").getAttribute("page_id");
-    console.log("keyword clicked: " + new_keyword);
-    console.log("page_id: " + page_id);
-    let pkg = {"new_keyword": new_keyword}
+window.onload = function(){
+    setup_display();
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://13.59.94.191/pages/" + page_id + "/");
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.responseType = "json";
-
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState === 4 && xhr.status <= 299){
-            console.log("SUCCESS");
-            //TODO remove all the keywords, the link, the instructions and change the url to green 
-            let del_me = keyword_link.closest(".make_relevant_content");
-            del_me.parentNode.style.color = "green";
-            del_me.parentNode.removeChild(del_me);
-        }//TODO should handle failure?
-    }
-    xhr.send(JSON.stringify(pkg));
-
-    return false;
+    document.querySelector("#user_name_submit").addEventListener("click",  event =>  user_name_click(event));
+    document.querySelector("#submit_button").addEventListener("click",     event =>  submit_button_click(event));
+    document.querySelector("#stop_session").addEventListener("click",      event =>  stop_session_click(event));
+    //handle dynamically added elements through bubbling
+    document.querySelector("#add_pages_visited").addEventListener("click", event => {
+        console.log("got a click in the add_pages_visited area");
+        let target = event.target;
+        if(target.classList.contains("make_relevant_button")){
+            console.log("routing the make_relevant button event to the appropriate function");
+            show_relevant_keywords(event);
+        }else if(target.classList.contains("keyword_link")){
+            console.log("routing the keyword_link event to the relevant function");
+            keyword_click(event);
+        }
+    });
 }
 
 /*
- * Clicking on a 'make relevant' link shows the list of keywords
- * each of which when clicked triggers an event to add them to the session.
+ * Prevent the user from inadvertantly closing the window.
  */
-function show_relevant_keywords(event){
-    let button_pressed = event.target;
-    let keywords = button_pressed.getAttribute("noun_keywords");
-    let instruction_div = document.createElement("div");
-    instruction_div.append("Select which of these keywords make it relevant:");
-    button_pressed.parentNode.append(instruction_div);
+let is_ongoing_session = false;
 
 
-    let keywords_list = keywords.split("~");
-    let keywords_unordered_list = document.createElement("ul");
-    for(x in keywords_list){
-        let keyword_list_item = document.createElement("li");
-        let keyword_link = document.createElement("a");
-        keyword_link.append(keywords_list[x]);
-        keyword_link.setAttribute("href", "#");
-        keyword_link.addEventListener("click", keyword_click);
-        keyword_list_item.append(keyword_link);
-        keywords_unordered_list.append(keyword_list_item);
+/*
+ * Prevent the user from closing the window by accident 
+ * if they have a session going on
+ */
+window.onbeforeunload = function(){
+    if(is_ongoing_session){
+        return "Closing this window will end your study session";
     }
-    button_pressed.parentNode.append(keywords_unordered_list);
-
-    //TODO make a cancel button that deletes this list
-    return false;
 }
+
+// ----------------------------------
+// message Listeners
+// -------------------------------------------------
 
 /*
  * Handle a summary text  message from the content page
@@ -114,9 +100,10 @@ function summary_text(msg, sender, sendResponse){
                 parent_div.classList.add("make_relevant_content");
                 let make_relevant_button = document.createElement("a");
                 make_relevant_button.setAttribute("href", "#");
+                make_relevant_button.classList.add("make_relevant_button");
                 make_relevant_button.append("Make relevant");
                 make_relevant_button.setAttribute("noun_keywords", xhr.response.keywords);
-                make_relevant_button.addEventListener("click", show_relevant_keywords);
+                //make_relevant_button.addEventListener("click", show_relevant_keywords);
                 parent_div.append(make_relevant_button);
                 page_visited.append(parent_div);
 
@@ -141,6 +128,100 @@ function summary_text(msg, sender, sendResponse){
     return true;
 }
 
+// ---------------------------------------------------
+// Event Listeners
+// ------------------------------------------------
+
+/*
+ * TODO get the page ID from the link data, not the session id from the stored information
+ *
+ * Clicking on a keyword sends an ajax request to make the page legit.
+ */
+function keyword_click(event){
+    let keyword_link = event.target;
+    let new_keyword = keyword_link.innerText;
+    let page_id = keyword_link.closest(".page_history").getAttribute("page_id");
+    console.log("keyword clicked: " + new_keyword);
+    console.log("page_id: " + page_id);
+    let pkg = {"new_keyword": new_keyword}
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://13.59.94.191/pages/" + page_id + "/");
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.responseType = "json";
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status <= 299){
+            console.log("SUCCESS");
+            //TODO remove all the keywords, the link, the instructions and change the url to green 
+            let del_me = keyword_link.closest(".make_relevant_content");
+            del_me.parentNode.style.color = "green";
+            del_me.parentNode.removeChild(del_me);
+        }//TODO should handle failure?
+    }
+    xhr.send(JSON.stringify(pkg));
+
+    return false;
+}
+
+/*
+ * Clicking stop_session button ends the session
+ *
+ * Here the background manages the state in case the 
+ * user tries to close the window right away
+ */
+function stop_session_click(){
+    let stop_time = new Date();
+    //TODO add a spinner?
+    chrome.runtime.sendMessage({
+        "type": "stop_session",
+        "stop_time": stop_time.getTime(),
+    }, response => {
+        if(!!response && !!response.success){
+            is_ongoing_session = false;
+            document.querySelector("#ongoing_study").style.display = "none";
+            document.querySelector("#collection_content").style.display = "contents";
+        }else{
+            console.log("errors");
+            document.querySelector("#error_content")
+                .innerText = "There was an error in closing this session";
+            document.querySelector("#error_content").style.display = "contents";
+        }
+        return true;
+    });
+}
+
+/*
+ * Clicking on a 'make relevant' link shows the list of keywords
+ * each of which when clicked triggers an event to add them to the session.
+ */
+function show_relevant_keywords(event){
+    let button_pressed = event.target;
+    let keywords = button_pressed.getAttribute("noun_keywords");
+    let instruction_div = document.createElement("div");
+    instruction_div.append("Select which of these keywords make it relevant:");
+    button_pressed.parentNode.append(instruction_div);
+
+
+    let keywords_list = keywords.split("~");
+    let keywords_unordered_list = document.createElement("ul");
+    for(x in keywords_list){
+        let keyword_list_item = document.createElement("li");
+        let keyword_link = document.createElement("a");
+        keyword_link.append(keywords_list[x]);
+        keyword_link.setAttribute("href", "#");
+        keyword_link.classList.add("keyword_link");
+        //keyword_link.addEventListener("click", keyword_click);
+        keyword_list_item.append(keyword_link);
+        keywords_unordered_list.append(keyword_list_item);
+    }
+    button_pressed.parentNode.append(keywords_unordered_list);
+
+    //TODO make a cancel button that deletes this list
+    return false;
+}
+
+
 /*
  * Put a spinner on top of the submission form
  */
@@ -156,22 +237,6 @@ function create_topic_submission_spinner(){
     spinner.style.left = my_rect.y;
     spinner.style.width = "150px";
     document.querySelector("body").append(spinner);
-}
-
-/*
- * Prevent the user from inadvertantly closing the window.
- */
-let is_ongoing_session = false;
-
-
-/*
- * Prevent the user from closing the window by accident 
- * if they have a session going on
- */
-window.onbeforeunload = function(){
-    if(is_ongoing_session){
-        return "Closing this window will end your study session";
-    }
 }
 
 /*
@@ -205,13 +270,84 @@ function set_user_name_get_user_id(user_name){
 }
 
 /*
+ * When a user clicks the submit button to start a new session
+ */
+function submit_button_click(){
+    let time_started = new Date();
+    let description = document.querySelector("[name=description]").value
+    let additional_keywords = document.querySelector(
+        "[name=additional_keywords]").value;
+    let duration = document.querySelector("[name=duration]").value;
+    if(!!description){
+        create_topic_submission_spinner();
+        chrome.runtime.sendMessage({
+            "type": "topic_submit",
+            "description": description + " . " + additional_keywords,
+            'additional_keywords': additional_keywords,
+            'duration': duration,
+            'time_started': time_started.getTime()
+        }, function(response){
+            console.log("got the response");
+            console.log(response);
+            //Remove the spinner so it doesn't show up later;
+            let topic_submission_spinner = document.querySelector("#topic_submission_spinner");
+            if(!!topic_submission_spinner){
+                topic_submission_spinner.parentNode.removeChild(topic_submission_spinner);
+            }
+
+            if(!!response && !!response.success){
+                is_ongoing_session = true;
+                document.querySelector("#ongoing_study")
+                    .style.display = "contents";
+                document.querySelector("#collection_content")
+                    .style.display = "none";
+            }else{
+                if(!!response && !!response.error){
+                    document.querySelector("#error_content").innerText = response.error;
+                    document.querySelector("#error_content").style.display = "contents";
+                }else{
+                    document.querySelector("#error_content").innerText = "Unkown Error";
+                    document.querySelector("#error_content").style.display = "contents";
+                }
+            }
+            return true;
+        })
+    }else{
+        console.log("errors");
+        document.querySelector("#error_content")
+            .innerText = "Please fill out both form fields";
+        document.querySelector("#error_content").style.display = "contents";
+    }
+}
+
+function user_name_click(){
+    let user_name = document.querySelector("#user_name_input").value;
+    if(user_name.length <= 1){ //ask again
+        console.log("No username");
+        document.querySelector("#user_name_input").focus();
+        document.querySelector("#user_name_submit").append("Please fill in a name and resubmit.");
+    }else{ //username is good
+        console.log("username is ", user_name);
+        set_user_name_get_user_id(user_name).then( user_id => {
+            console.log(user_id);
+            document.getElementById("user_name_form").style.display = "none";
+            document.querySelector("#user_name_greeting").innerText = user_name;
+            document.getElementById("greeting").style.display = "contents";
+            document.getElementById("collection_content").style.display = "contents";
+        }).catch( err => {
+            console.log(err);
+            //TODO something on the UI to show that the AJAX call failed
+        });
+    }
+}
+
+/*
  * When the page loads first check whether there's an ongoing session (unlikely)
  * and whether the user has already logged in before and provided a name. If they
  * haven't then ask them for their name.
  *
- * Then set all of the event handlers
  */
-window.onload = function(){
+function setup_display(){
     chrome.storage.sync.get(["end_time", "user_name", "user_id"], result => {
         //If there's an ongoing study session then show the ongoing dialog
         if(!!result && result.end_time && new Date() < new Date(result.end_time)){
@@ -228,106 +364,4 @@ window.onload = function(){
             document.querySelector("#user_name_form").style.display = "contents";
         }
     });
-
-    document.querySelector("#user_name_submit").addEventListener("click", event => {
-        let user_name = document.querySelector("#user_name_input").value;
-        if(user_name.length <= 1){ //ask again
-            console.log("No username");
-            document.querySelector("#user_name_input").focus();
-            document.querySelector("#user_name_submit").append("Please fill in a name and resubmit.");
-        }else{ //username is good
-            console.log("username is ", user_name);
-            set_user_name_get_user_id(user_name).then( user_id => {
-                console.log(user_id);
-                document.getElementById("user_name_form").style.display = "none";
-                document.querySelector("#user_name_greeting").innerText = user_name;
-                document.getElementById("greeting").style.display = "contents";
-                document.getElementById("collection_content").style.display = "contents";
-            }).catch( err => {
-                console.log(err);
-                //TODO something on the UI to show that the AJAX call failed
-            });
-        }
-    });
-
-    /*
-     * When a user clicks the submit button then get the values, make sure they're not empty
-     * and send a message to the background so that it can start the session and everything.
-     *
-     * TODO this should be the topic_submit_button
-     */
-    document.querySelector("#submit_button").addEventListener("click", event => {
-        let time_started = new Date();
-        let description = document.querySelector("[name=description]").value
-        let additional_keywords = document.querySelector(
-            "[name=additional_keywords]").value;
-        let duration = document.querySelector("[name=duration]").value;
-        if(!!description){
-            create_topic_submission_spinner();
-            chrome.runtime.sendMessage({
-                "type": "topic_submit",
-                "description": description + " . " + additional_keywords,
-                'additional_keywords': additional_keywords,
-                'duration': duration,
-                'time_started': time_started.getTime()
-            }, function(response){
-                console.log("got the response");
-                console.log(response);
-                //Remove the spinner so it doesn't show up later;
-                let topic_submission_spinner = document.querySelector("#topic_submission_spinner");
-                if(!!topic_submission_spinner){
-                    topic_submission_spinner.parentNode.removeChild(topic_submission_spinner);
-                }
-
-                if(!!response && !!response.success){
-                    is_ongoing_session = true;
-                    document.querySelector("#ongoing_study")
-                        .style.display = "contents";
-                    document.querySelector("#collection_content")
-                        .style.display = "none";
-                }else{
-                    if(!!response && !!response.error){
-                        document.querySelector("#error_content").innerText = response.error;
-                        document.querySelector("#error_content").style.display = "contents";
-                    }else{
-                        document.querySelector("#error_content").innerText = "Unkown Error";
-                        document.querySelector("#error_content").style.display = "contents";
-                    }
-                }
-                return true;
-            })
-        }else{
-            console.log("errors");
-            document.querySelector("#error_content")
-                .innerText = "Please fill out both form fields";
-            document.querySelector("#error_content").style.display = "contents";
-        }
-    });
-
-    /**
-     * Clicking the "complete session" button makes the session end.
-     * No more sending things and the user is able to start a new session.
-     *
-     * TODO add   media_recorder.stop();
-     */
-     document.querySelector("#stop_session").addEventListener("click", event => {
-         let stop_time = new Date();
-         //TODO add a spinner?
-         chrome.runtime.sendMessage({
-             "type": "stop_session",
-             "stop_time": stop_time.getTime(),
-         }, response => {
-            if(!!response && !!response.success){
-                is_ongoing_session = false;
-                document.querySelector("#ongoing_study").style.display = "none";
-                document.querySelector("#collection_content").style.display = "contents";
-            }else{
-                console.log("errors");
-                document.querySelector("#error_content")
-                    .innerText = "There was an error in closing this session";
-                document.querySelector("#error_content").style.display = "contents";
-            }
-             return true;
-         });
-     });
 }
