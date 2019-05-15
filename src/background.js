39d0ab651d75608cdb805608d177c69f8309becb
Barnaby-at-020885411870 <barnaby@ahuraai.com>
2019-05-15 14:40:33 -0400

    adding code for how the recurring alarm will work

--- src/background.js
+++ src/background.js
@@ -44,9 +44,24 @@ chrome.storage.local.get(['user_id', 'user_name'], result => {
     }
 });
 
+/*
+ * Determine whether the time passed in is past the current time.
+ *
+ * @param alarm_time - after when are we supposed to sound the alarm today? Should be in the format 00:00:00
+ */
+function is_past_time(alarm_time){
+    const cur_time = new Date();
+    const cur_hour = cur_time.getHour();
+    const cur_min = cur_time.getMinutes();
+    const alarm_hour = parseInt(alarm_time.split(":")[0])
+    const alarm_min = parseInt(alarm_time.split(":")[1])
+    return cur_hour + (cur_min * 0.01) >= alarm_hour + (cur_min * 0.01);
+}
+
 
 /*
  * Determine the day of the week, and get todays alarm time
+ * TODO code review this method. It's got a LOT of nesting, perhaps more than it needs.
  */
 function get_todays_alarm_time(){
     return new Promise((resolve, reject) => {
@@ -68,9 +83,9 @@ function get_todays_alarm_time(){
                 resolve(result.alarm_times[cur_day])
             }else{
                 fetch(`${globals.api_url}/users/${result.user_id}/goals/`).then( get_response => {
-                    if(!!response.ok && !!response.json()){
+                    if(!!get_response.ok && !!get_response.json()){
                         chrome.storage.local.set({"alarm_times": get_response.json()});
-                        resolve(default_alarm_times[cur_day]);
+                        resolve(get_response.json()[cur_day]);
                     }else{
                         fetch(`${globals.api_url}/users/${result.user_id}/goals/`,{
                             headers: {
@@ -79,12 +94,12 @@ function get_todays_alarm_time(){
                             credentials: "same-origin",
                             method: "POST",
                             body: JSON.stringify(default_alarm_times)
-                        }).then( response => {
-                            if(!response.ok){
+                        }).then( post_response => {
+                            if(!post_response.ok){
                                 console.log("for some reason the server did not save the default goal settings");
                             }
-                            chrome.storage.local.set({"alarm_times": default_alarm_times});
-                            resolve(default_alarm_times[cur_day]);
+                            chrome.storage.local.set({"alarm_times": post_response.json()});
+                            resolve(post_response.json()[cur_day]);
                         });
                     }
                 });
@@ -100,10 +115,9 @@ chrome.alarms.create("study_time", {periodInMinutes: 15});
 //If it is then open up the window
 chrome.alarms.onAlarm.addListener( alarm => {
     if(alarm.name === "study_time"){
-        //TODO if the alarm time in the database is equal to or greater than the alarm time.
-            //TODO which day of the week is it, what time is our alarm supposed to be today?
-            //TODO then if there isn't an ongoing session, or todays total session time is greater than the goal,
-            //TODO then open up the app window
+        get_todays_alarm_time().then( alarm_time => {
+            if(is_past_time(alarm_time) && !!chat_window){ //TODO also check whether there has been a study session today (and how long the sessions lasted today)
+                open_window();
             }
         });
     }
